45860371f668bb8b13de70302b79b8d3f9ee5052
asif-hussain-ansari <asifali235yyr6@gmail.com>
2026-06-16 17:07:44 +0500

    task 2 done

---
+++ script2.js
@@ -0,0 +1,24 @@
+let marks=prompt("Enter marks")
+
+marks=Number(marks)
+
+if(marks>=90){
+    console.log("A1 grade");
+    
+}
+else if(marks>=80){
+    console.log("A grade");
+    
+}
+else if(marks>=70){
+    console.log("B grade");
+    
+}
+else if(marks>=60){
+    console.log("C grade");
+    
+}
+else{
+    console.log("you are fail");
+    
+}
